y as 1D gave wrong results.. Now numpyro works :-|

bayes_opt_experiments/1D_v1_bayesopt_4_methods.py
@@ -9,38 +9,45 @@ from src.optimization.bayesian_optimization import BayesianOptimization
 from src.regression_models.SPN_regression2 import SumProductNetworkRegression
 from src.regression_models.gaussian_process_regression import GaussianProcess_sklearn
 from src.regression_models.numpyro_neural_network import NumpyroNeuralNetwork
+from src.regression_models.stan_neural_network import StanNeuralNetwork
 #from src.regression_models.gaussian_mixture_regression2 import GMRegression
 from src.regression_models.naive_GMR import NaiveGMRegression
 from src.regression_models.bohamiann import BOHAMIANN
 
-from src.benchmarks.go_benchmark_functions.go_funcs_S import Step
+from src.benchmarks.go_benchmark_functions.go_funcs_S import Step, Schwefel26
 from src.benchmarks.go_benchmark_functions.go_funcs_W import Weierstrass
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
-from src.benchmarks.custom_test_functions.problems import SimonsTest,SimonsTest3_cosine_fuction, SimonsTest4_cosine_fuction
+from src.benchmarks.custom_test_functions.problems import SimonsTest,SimonsTest0,SimonsTest3_cosine_fuction, SimonsTest4_cosine_fuction
 
 problem = SimonsTest4_cosine_fuction()
-
+problem = SimonsTest()
+problem = SimonsTest0()
+problem = Schwefel26(dimensions=1)
+problem = Step(dimensions=1)
 np.random.seed(2)
 
-BOHAMIANN_regression = BOHAMIANN(num_warmup = 200, num_samples = 300, num_keep_samples= 300)
+BOHAMIANN_regression = BOHAMIANN(num_warmup= 200, num_samples=200, num_keep_samples= 50)
 GP_regression = GaussianProcess_sklearn()
-NNN_regression = NumpyroNeuralNetwork(num_chains = 4, num_warmup= 200, num_samples=300, num_keep_samples= 50)
+NNN_regression = NumpyroNeuralNetwork(num_chains = 4, num_warmup= 2000, num_samples=2000, num_keep_samples= 300, extra_name="2000-2000")
+NNN_regression = NumpyroNeuralNetwork(num_chains = 4, num_warmup= 200, num_samples=200, num_keep_samples= 200, extra_name="200-200")
+BNN_regression = StanNeuralNetwork()
 SPN_reg = SumProductNetworkRegression(optimize=True, manipulate_variance=True)
 SPN_reg2 = SumProductNetworkRegression(optimize=False, manipulate_variance=True)
-GM_reg = NaiveGMRegression(optimize=False, manipulate_variance=True)
+GM_reg = NaiveGMRegression(optimize=False, manipulate_variance=False)
 
-regression_models = [GP_regression,BOHAMIANN_regression, GM_reg,SPN_reg2 ]
+regression_models = [GP_regression,NNN_regression, GM_reg,BNN_regression ]
+regression_models = [GP_regression,NNN_regression, GM_reg,SPN_reg2 ]
 plt.figure(figsize=(12, 8))
 outer_gs = gridspec.GridSpec(2, 2)
 
 BOs = []
 opts = []
 for i in range(len(regression_models)):
-    BOs.append(BayesianOptimization(problem, regression_models[i]))
+    BOs.append(BayesianOptimization(problem, regression_models[i], init_n_samples=10))
     opts.append(OptimizationStruct())
 
 for iter in range(20):

bayes_opt_experiments/1D_v2_bayesopt_GMR.py
@@ -22,7 +22,7 @@ problem = SimonsTest(dimensions=1)
 
 np.random.seed(2)
 
-GM_reg = NaiveGMRegression(optimize=False, manipulate_variance=True)
+GM_reg = NaiveGMRegression(optimize=False, manipulate_variance=False)
 
 regression_models = [GM_reg]
 plt.figure(figsize=(12, 8))
@@ -48,8 +48,8 @@ for iter in range(50):
         BO.plot_surrogate_and_expected_improvement(ax, opt, show_name=True)
         opts[i] = opt
         number = f"{iter}".zfill(3)
-    plt.savefig(f"{path}/{fig_name}_{number}.jpg")
-    #plt.show()
+    #plt.savefig(f"{path}/{fig_name}_{number}.jpg")
+    plt.show()
 
 x_hist,y_hist = BO.get_optimization_hist()
 print(x_hist)

src/benchmarks/custom_test_functions/problems.py
@@ -70,8 +70,12 @@ class SimonsTest3_cosine_fuction(Benchmark):
             k = -np.round(x)
 
         self.nfev += 1
-        return x*sin(k*x)+k
 
+        result = x*sin(k*x)+k
+        if result.ndim == 0:
+            return result
+        else:
+            return result[0]
 
 class SimonsTest2(Benchmark):
 
@@ -107,6 +111,27 @@ class SimonsTest(Benchmark):
         x = x+ 0.5
         self.nfev += 1
         result = 0.5 * (np.sign(x-0.5) + 1)+np.sin(100*x)*0.1
+        if result.ndim == 0:
+            return result
+        else:
+            return result[0]
+
+class SimonsTest0(Benchmark):
+
+    def __init__(self, dimensions=1):
+        Benchmark.__init__(self, dimensions)
+        assert dimensions == 1
+        self._bounds = list(zip([-0.5] * self.N,
+                           [0.5] * self.N))
+        self.custom_bounds = ([-5, 5], [-5, 5])
+
+        self.global_optimum = [[0. for _ in range(self.N)]]
+        self.fglob = 0.0
+        self.change_dimensionality = True
+    def fun(self,x, *args): 
+        x = x+ 0.5
+        self.nfev += 1
+        result = 0.5 * (np.sign(x-0.5) + 1)
         if result.ndim == 0:
             return result
         else:

src/optimization/bayesian_optimization.py
@@ -14,10 +14,10 @@ if __name__=="__main__":
     from src.regression_models.numpyro_neural_network import NumpyroNeuralNetwork #JAX
     from src.regression_models.gaussian_process_regression import GaussianProcess_sklearn, GaussianProcess_pyro
     from src.regression_models.bohamiann import BOHAMIANN #Torch
-    #from src.regression_models.gaussian_mixture_regression2 import GMRegression
+    from src.regression_models.naive_GMR import NaiveGMRegression
     from src.regression_models.SPN_regression2 import SumProductNetworkRegression
     from src.regression_models.mean_regression import MeanRegression
-    from src.benchmarks.custom_test_functions import SimonsTest3_cosine_fuction
+    from src.benchmarks.custom_test_functions.problems import SimonsTest3_cosine_fuction
 
 PLOT_NR = 0
 
@@ -44,14 +44,16 @@ class BayesianOptimization(PlottingClass):
         self.model.fit(X_init,Y_init)
 
     def _initXY(self, sample_size):
+        np.random.seed(2)
         X_init = np.random.uniform(*self.bounds,size = (sample_size,self.problem_dim))
         try:
             Y_init = self.obj_fun(X_init)
         except:
-            y = []
-            for x in X_init:
-                y.append(self.obj_fun(x))
-            Y_init = np.array(y)[:,None]
+            assert False
+            # y = []
+            # for x in X_init:
+            #     y.append(self.obj_fun(x))
+            # Y_init = np.array(y)[:,None]
         return X_init,Y_init
     def predict(self,X, gaussian_approx = True, get_px = False):
         if get_px:
@@ -67,14 +69,19 @@ class BayesianOptimization(PlottingClass):
     def expected_improvement(self,X,xi=0, return_analysis = False):
         assert X.ndim == 2
         #print("OBS X[:,None] might fail in largers dims!")
-        mu, sigma, p_x = self.predict(X, get_px=True) #Partial afledt. Pytorch. 
+        if self.model.name == "Naive Gaussian Mixture Regression":
+            mu, sigma, p_x = self.predict(X, get_px=True) #Partial afledt. Pytorch. 
+        else:
+            mu, sigma = self.predict(X) #Partial afledt. Pytorch. 
+
         imp = -mu - self.f_best - xi
         Z = imp/sigma
         exploitation = imp*norm.cdf(Z)
         exploration = sigma*norm.pdf(Z)
         EI = exploitation + exploration
-        N = self._X.shape[0]
-        EI *= 1/(N*p_x)
+        if self.model.name == "Naive Gaussian Mixture Regression":
+            N = self._X.shape[0]
+            EI *= 1/(N*p_x)
         #EI = exploitation/10 + exploration
         if return_analysis:
             return EI, exploitation, exploration
@@ -259,18 +266,18 @@ if __name__ == "__main__":
 
     bounds = [0,1]
     #datasize = int(input("Enter number of datapoints: "))
-    datasize = 10
-    np.random.seed(2)
-    X_sample =  np.random.uniform(*bounds,size = (datasize,1))
-    Y_sample = obj_fun(X_sample)
-
+    # datasize = 10
+    # np.random.seed(2)
+    # X_sample =  np.random.uniform(*bounds,size = (datasize,1))
+    # Y_sample = obj_fun(X_sample)
+    problem = SimonsTest3_cosine_fuction()
     mean_regression = MeanRegression()
-    SPN_regression = SumProductNetworkRegression(manipulate_varance=False, optimize=True)
+    SPN_regression = SumProductNetworkRegression(manipulate_variance=False, optimize=True)
     # GP_regression = GaussianProcess_sklearn()
     # GP_regression2 = GaussianProcess_pyro(noise=0)
     BOHAMIANN_regression = BOHAMIANN(num_warmup = 200, num_samples = 400)  
     NNN_regression = NumpyroNeuralNetwork(num_chains = 4, num_warmup= 200, num_samples=200, num_keep_samples= 50)
-    mixture_regression = GMRegression()
+    mixture_regression = NaiveGMRegression()
     
     #regression_model = [mixture_regression, GP_regression,BOHAMIANN_regression,NNN_regression]
     regression_models = [SPN_regression, mixture_regression,BOHAMIANN_regression, mean_regression]
@@ -282,7 +289,7 @@ if __name__ == "__main__":
     plt.figure(figsize=(12, 8))
     outer_gs = gridspec.GridSpec(2, len(regression_models)//2+1)
     for i in range( len(regression_models)):
-        BO_BNN = BayesianOptimization(obj_fun, regression_models[i],bounds,X_sample,Y_sample)
+        BO_BNN = BayesianOptimization(problem, regression_models[i])
         opt = BO_BNN.optimization_step()
         BO_BNN.plot_surrogate_and_expected_improvement(outer_gs[i],opt, show_name=True)
     plt.show()

src/regression_models/numpyro_neural_network.py
@@ -5,7 +5,7 @@ from numpyro import sample
 from numpyro.infer import MCMC, NUTS,Predictive
 import numpy as np
 
-from ..utils import normalize, denormalize
+from src.utils import normalize, denormalize
 
 from jax import vmap
 import jax.numpy as jnp
@@ -75,6 +75,7 @@ class NumpyroNeuralNetwork:
         else:
             sigma_obs = sample("sigma", dist.Exponential(jnp.ones((1,1))*self.obs_variance_prior))+0.00001
 
+        #sigma_obs = 0.000001
         #likelihood 
         z1 = jnp.tanh(b1+jnp.matmul(X, w1))  # <= first layer of activations
         z2 = jnp.tanh(b2+jnp.matmul(z1, w2))  # <= second layer of activations
@@ -82,14 +83,16 @@ class NumpyroNeuralNetwork:
         with numpyro.plate("data", N):
             # note we use to_event(1) because each observation has shape (1,)
             numpyro.sample("Y", dist.Normal(z3, sigma_obs).to_event(1), obs=Y)
+            #numpyro.sample("Y", dist.Delta(z3).to_event(1), obs=Y)
 
         return z3, sigma_obs
     
-    def fit(self, X, Y, verbose = False): #run_inference
+    def fit(self, X, Y, verbose = True): #run_inference
         assert X.ndim == 2
+        assert Y.ndim == 2
 
-        X_, self.x_mean, self.x_std = normalize(X)
-        Y_, self.y_mean, self.y_std = normalize(Y)
+        X, self.x_mean, self.x_std = normalize(X)
+        Y, self.y_mean, self.y_std = normalize(Y)
 
         start = time.time()
         kernel = NUTS(self.model, target_accept_prob=self.target_accept_prob)
@@ -100,7 +103,7 @@ class NumpyroNeuralNetwork:
             num_chains=self.num_chains,
             progress_bar=False if "NUMPYRO_SPHINXBUILD" in os.environ else True,
         )
-        mcmc.run(self.rng_key, X_, Y_)
+        mcmc.run(self.rng_key, X, Y)
         if verbose:
             mcmc.print_summary()
             print("\nMCMC elapsed time:", time.time() - start)

src/regression_validation/regression_validation.py
@@ -1,13 +1,13 @@
 import numpy as np
 from src.regression_models.numpyro_neural_network import NumpyroNeuralNetwork
+from src.regression_models.stan_neural_network import StanNeuralNetwork
 from src.regression_models.gaussian_process_regression import GaussianProcess_sklearn
 from src.regression_models.bohamiann import BOHAMIANN
 #from src.regression_models.gaussian_mixture_regression2 import GMRegression
 from src.regression_models.mean_regression import MeanRegression
 from src.regression_models.SPN_regression2 import SumProductNetworkRegression
 
-import random
-import numpy as np
+#import random
 from src.utils import RegressionValidation
 
 from src.benchmarks.custom_test_functions.problems import SimonsTest,SimonsTest3_cosine_fuction, SimonsTest4_cosine_fuction
@@ -22,8 +22,8 @@ import os
 from datetime import datetime
 
 #prob = Zirilli(dimensions = 2)
-dims = [2,3,5,10]
-#dims = [1]
+#dims = [2,3,5,10]
+dims = [1]
 
 problems = [Step(dimensions = x) for x in dims]
 problems += [Rastrigin(dimensions = x) for x in dims]
@@ -34,14 +34,14 @@ problems += [Schwefel26(dimensions = x) for x in dims]
 
 #problems = [SimonsTest4_cosine_fuction()]
 
-random.seed()
-random.shuffle(problems)
+# random.seed()
+# random.shuffle(problems)
 
 # BOHAMIANN_regression_fast = BOHAMIANN(num_warmup = 200, num_samples = 300, num_keep_samples= 100)
 # BOHAMIANN_regression = BOHAMIANN(num_warmup = 2000, num_samples = 2000, num_keep_samples= 300,  extra_name="2000-2000")
 # BOHAMIANN_regression_slow = BOHAMIANN(num_warmup = 4000, num_samples = 4000, num_keep_samples= 300,  extra_name="4000-4000")
-# NNN_regression_fast = NumpyroNeuralNetwork(num_chains = 4, num_warmup= 200, num_samples=300, num_keep_samples= 100)
-# NNN_regression = NumpyroNeuralNetwork(num_chains = 4, num_warmup= 2000, num_samples=2000, num_keep_samples= 300, extra_name="2000-2000")
+NNN_regression_fast = NumpyroNeuralNetwork(num_chains = 4, num_warmup= 200, num_samples=300, num_keep_samples= 100)
+NNN_regression = NumpyroNeuralNetwork(num_chains = 4, num_warmup= 2000, num_samples=2000, num_keep_samples= 300, extra_name="2000-2000")
 # # Slow NNN with 3*50 hidden units takes 15 min per. training 
 # # -> way to slow for these experiements. 
 
@@ -52,22 +52,24 @@ random.shuffle(problems)
 # #regression_models = [MeanRegression()]
 # regression_models = [GaussianProcess_sklearn()]
 regression_models = [SumProductNetworkRegression(manipulate_variance=False, optimize=True, tracks=2, channels=50)]
+regression_models = [NNN_regression_fast]
+#regression_models = [StanNeuralNetwork()]
 ## Data enrichment ##
 #include_true_values(problems, remove_min_n_test=True)
 
 # unit test ###
-test_model = MeanRegression()
-for problem in problems:
-    print(test_model.name, f"{type(problem).__name__} in dim {problem.N}")
-    RV = RegressionValidation(problem, test_model, 2)
-    RV.train_test_loop([10], 1)
-print("\n-- TEST: all problems are defined --\n")
+# test_model = MeanRegression()
+# for problem in problems:
+#     print(test_model.name, f"{type(problem).__name__} in dim {problem.N}")
+#     RV = RegressionValidation(problem, test_model, 2)
+#     RV.train_test_loop([10], 1)
+# print("\n-- TEST: all problems are defined --\n")
 
-for regression_model in regression_models:
-    print(regression_model.name, f"{type(problem).__name__}")
-    RV = RegressionValidation(problems[0], regression_model, 2)
-    RV.train_test_loop([10], 1)
-print("\n-- TEST: all models are defined --\n")
+# for regression_model in regression_models:
+#     print(regression_model.name, f"{type(problem).__name__}")
+#     RV = RegressionValidation(problems[0], regression_model, 2)
+#     RV.train_test_loop([10], 1)
+# print("\n-- TEST: all models are defined --\n")
 
 ### main ###
 n_train_array = [int(x) for x in np.logspace(1, 2.5, 9)]
@@ -75,6 +77,7 @@ n_train_array = [int(x) for x in np.logspace(1, 2.5, 9)]
 # n_train_array = [10,20,30]
 #n_train_array = [int(x) for x in np.logspace(1, 1.8, 9)]
 n_test = 10000
+n_test = 10
 
 run_name = datetime.today().strftime('%m%d_%H%M')
 dirname=os.path.dirname
@@ -85,7 +88,7 @@ try:
 except:
     print(f"Couldn't create {path}")
 
-np.random.seed()
+#np.random.seed()
 for problem in problems:
     for random_seed in np.random.randint(9999, size=1):
         for regression_model in regression_models:

src/utils.py
@@ -1,5 +1,3 @@
-from cProfile import label
-from turtle import color
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from matplotlib import cm
@@ -92,7 +90,7 @@ class PlottingClass:
         ax1 = plt.subplot(gs[0])
         ax2 = plt.subplot(gs[1])
         self.plot_regression_gaussian_approx(ax1,opt.Xgrid, show_name = show_name)
-        self.plot_regression_credible_interval(ax1,opt.Xgrid)
+        #self.plot_regression_credible_interval(ax1,opt.Xgrid)
         X_true =  np.linspace(*self.bounds,10000)
         Y_true = self.obj_fun(X_true)
         ax1.plot(X_true, Y_true, "--", color="Black")
@@ -183,7 +181,7 @@ class RegressionValidation(PlottingClass):
             self.data_generator(n_train, n_test_points)
             X,y=self.train_X, self.train_y
             X_test,y_test = self.test_X, self.test_y 
-            self.model.fit(X,y)
+            self.model.fit(X,y[:,None])
             Y_mu,Y_sigma,_ = self.model.predict(X_test)
             self.mean_abs_pred_error.append(np.mean(np.abs(y_test-Y_mu)))
             self.mean_rel_pred_error.append(