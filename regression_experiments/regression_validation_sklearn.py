ec6f52bbb613b56cd6ee3dae015b52de39699205
Simon Kruse <s164221@student.dtu.dk>
2022-06-17 12:20:29 +0200

    update

--- regression_experiments/regression_validation_sklearn.py
+++ regression_experiments/regression_validation_sklearn.py
@@ -24,20 +24,18 @@ reg_models = [MeanRegression(),
             BOHAMIANN(num_keep_samples=500), 
             NumpyroNeuralNetwork(num_warmup=500,num_samples=500,
                                 num_chains=4),
-            SumProductNetworkRegression(optimize=True),
-            GMRegression(optimize=True)]
+            SumProductNetworkRegression(optimize=True)]
+            #GMRegression(optimize=True)]
             
 # random.seed()
 # random.shuffle(reg_models)
 #reg_models = [SumProductNetworkRegression(optimize=False)]#, 
 #reg_models = [SumProductNetworkRegression(optimize=False, opt_n_iter=10, extra_name="no-optimized-low-test")]
-#reg_models = [GMRegression(optimize=False,opt_n_iter=10, extra_name="no-optimized-low-test")]
+reg_models = [GMRegression(optimize=True)]
 ### main ###
 n_train_array = [int(x) for x in np.logspace(1, 2.5, 9)]
 n_test = 10000
 #n_test = 1000
-
-n_train_array = [10]
 
 
 run_name = datetime.today().strftime('%m%d_%H%M')
@@ -54,12 +52,9 @@ except:
 # problems += [SimonsTest2_probibalistic2()]
 problems = [Test1(),Test2(),Test3(),Test4(),Test3b()]
 #for regression_model in reg_models:
-for problem in problems:
-    np.random.seed()
-    for random_seed in np.random.randint(99999, size=1):
-        #for problem in problems:
+np.random.seed()
+for random_seed in np.random.randint(99999, size=5):
+    for problem in problems:
         for regression_model in reg_models:
-            #print(regression_model.name, f"{name_problem} in {dim}")
-            
             RV = RegressionTest_sklearn(regression_model,problem, random_seed)
             RV.train_test_loop(n_train_array, n_test, output_path = f"{path}")
